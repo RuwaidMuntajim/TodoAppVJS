46b24c8d334f855d64131f85970e1162600d4ec8
Ruwaid Muntajim <68751420+RuwaidMuntajim@users.noreply.github.com>
2021-11-06 00:47:40 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,56 @@
+// get reference
 const todos = document.querySelector('#todolist');
 const form = document.querySelector('#form');
+const select = document.querySelector('#car');
 
+
+// function for rendering todos fetched from firestore
 const renderTodos = (doc) => {
     let li = document.createElement('li');
     let todo = document.createElement('span');
     let cross = document.createElement('span');
-    
+    let correct = document.createElement('i');
+    let div = document.createElement('div');
+
     cross.setAttribute('id', 'cross')
-    cross.textContent = 'x'
+    div.setAttribute('class', 'li-btn');
     li.setAttribute('data-id', doc.id);
+    correct.setAttribute('class', 'fas fa-check-square');
 
     todo.textContent = doc.data().todo;
-    li.appendChild(todo);
-    li.appendChild(cross);
-    todos.appendChild(li);
+    cross.textContent = 'x';
 
+
+    li.appendChild(todo);
+    div.appendChild(correct);
+    div.appendChild(cross);
+    li.appendChild(div);
+    todos.appendChild(li);
+    // Event listener to delete selected todo
     cross.addEventListener('click', (e) => {
         e.stopPropagation();
-        let id = e.target.parentElement.getAttribute('data-id');
+        let id = e.target.parentElement.parentElement.getAttribute('data-id');
         db.collection('todolist').doc(id).delete();
     })
+    // Event listener to identify selected todo as completed
+    correct.addEventListener('click', (e) => {
+        let id = e.target.parentElement.parentElement.getAttribute('data-id')
+        db.collection('todolist').doc(id).update({
+            completed: true
+        })
+    });
+}
+
+// rendering specific todos 
+const renderPartial = (doc) => {
+    let li = document.createElement('li');
+    let todo = document.createElement('span');
+
+    li.setAttribute('data-id', doc.id);
+    todo.textContent = doc.data().todo;
+
+    li.appendChild(todo);
+    todos.appendChild(li);
 }
 
 /*db.collection('todolist').get().then(snapshot => {
@@ -29,23 +60,65 @@ const renderTodos = (doc) => {
     })
 }) */
 
+// add todo
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     db.collection('todolist').add({
-        todo: e.target.name.value
+        todo: e.target.name.value,
+        completed: false
     })
     form.name.value = "";
-})
+});
+
+
+
+
+// realtime Listener
 
 db.collection('todolist').onSnapshot(snapshot => {
-    console.log(snapshot.docChanges());
     let changes = snapshot.docChanges();
     changes.forEach(change => {
         if (change.type == "added") {
-            renderTodos(change.doc)
-        } else if (change.type == "removed") {
+            if (select.options[select.selectedIndex].value == "All" || select.options[select.selectedIndex].value == "Uncompleted") {
+                renderTodos(change.doc)
+            }
+        }
+        else if (change.type == "removed") {
             let li = document.querySelector(`[data-id=${change.doc.id}]`)
             todos.removeChild(li);
-        } 
-    })
-})
+        }
+    });
+});
+
+
+
+// conditional UI rendering
+
+select.addEventListener('change', (e) => {
+    todos.textContent = "";
+    if (select.options[select.selectedIndex].value == "Completed") {
+        db.collection('todolist').where('completed', '==', true).get().then(snapshot => {
+            let docs = snapshot.docs;
+            docs.forEach(doc => {
+                
+                renderPartial(doc);
+            })
+        })
+    } else if (select.options[select.selectedIndex].value == "Uncompleted") {
+        db.collection('todolist').where('completed' , '==', false).get().then(snapshot => {
+            let docs = snapshot.docs;
+            docs.forEach(doc => {
+                renderPartial(doc);
+            })
+        })
+    } else {
+        db.collection('todolist').get().then(snapshot => {
+            let docs = snapshot.docs;
+            docs.forEach(doc => {
+                renderTodos(doc);
+            })
+        })
+    }
+});
+// The End :)
